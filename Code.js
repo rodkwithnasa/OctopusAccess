647d8fedcfdf3e7dd47e7ce76e65106cb83e90cc
rodkwithnasa <34109135+rodkwithnasa@users.noreply.github.com>
2026-02-11 11:10:20 +0000

    uses queries to get average CV per charge rate and calculate subsequent cost

--- Code.js
+++ Code.js
@@ -106,6 +106,8 @@ const elecImportFormat = row => {
 const gasTariffCode = 'GasTariff';
 const dd = 'DIRECT_DEBIT';
 const gasQueryString = `query(myTable,"SELECT Col1 where Col6 = '${gasTariffCode}' and Col4 = '${dd}' and ((datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' < Col3 and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2) or ( Col3 is null and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2))")/100`;
+const avCalValueNew = `rounddown(query(query(Table3,"SELECT D,AVG(A) GROUP BY D"),"SELECT Col2 WHERE Col1 =" & R[0]C[-1],0),1)`;
+const gasCostString = `(((R[0]C[-4] * 1.02264 * ${avCalValueNew})/3.6)*R[0]C[-1])`;
 const gasStTariffCode = 'GasTariffST';
 const gasStQueryString = `query(myTable,"SELECT Col1 where Col6 = '${gasStTariffCode}' and Col4 = '${dd}' and ((datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' < Col3 and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2) or ( Col3 is null and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2))")/100`;
 /**
@@ -114,7 +116,7 @@ const gasStQueryString = `query(myTable,"SELECT Col1 where Col6 = '${gasStTariff
  * @returns formatted row
  */
 const gasFormat = row => {
-            const formattedRow = [row[0],gSheetToDate(row[1]),gSheetToDate(row[2]),gasQueryString,'(((R[0]C[-4] * 1.02264 * AvCalValueNew)/3.6)*R[0]C[-1])',"Gas"];
+            const formattedRow = [row[0],gSheetToDate(row[1]),gSheetToDate(row[2]),gasQueryString,gasCostString,"Gas"];
             const outputRows = [formattedRow];
             if (checkDayStart(row[1])) {
               const extraRow = [1,gSheetToDate(row[1]),gSheetToDate(row[1]),gasStQueryString,'(R[0]C[-4]*R[0]C[-1])',"GStanding"];
@@ -206,6 +208,7 @@ function parseDMY(value) {
         y = parseInt(date[2], 10);
     return (Number.isNaN(d) || Number.isNaN(m)|| Number.isNaN(y))?  null : new Date(y, m - 1, d);
 }
+const cfQuery = `=${gasQueryString}`;
 /**
  * Returns the formatted rows for calorific value
  * @param row
@@ -213,7 +216,7 @@ function parseDMY(value) {
  */
 const calorificFormat = row => {
             const event = parseDMY(row[2]);
-            const formattedRow = [row[0],gSheetToDate(event.toISOString()),gSheetToDate(event.toISOString()),0,0,row[5]];
+            const formattedRow = [row[0],gSheetToDate(event.toISOString()),gSheetToDate(event.toISOString()),cfQuery,0,row[5]];
             const outputRows = [formattedRow];
             return outputRows;
           }
