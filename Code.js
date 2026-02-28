1996d2ba1349356b8add1c722a953ec3b6c4df3a
rodkwithnasa <34109135+rodkwithnasa@users.noreply.github.com>
2026-02-28 08:34:23 +0000

    Previous fix had made export charge +ve - changed to -ve as before

--- Code.js
+++ Code.js
@@ -126,7 +126,7 @@ const gasFormat = row => {
           }
 const eETariffCode = 'ElecExportTariff';
 //const eEqueryString = `query(myTable,"SELECT Col1 where Col6 = '${eETariffCode}' and ( Col3 is null and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2)")*-1/100`;
-const eEqueryString = `query(myTable,"SELECT Col1 where Col6 = '${eETariffCode}' and ((datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' < Col3 and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2) or ( Col3 is null and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2))")/100`;
+const eEqueryString = `query(myTable,"SELECT Col1 where Col6 = '${eETariffCode}' and ((datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' < Col3 and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2) or ( Col3 is null and datetime '" & TEXT(R[0]C[-2], "yyyy-mm-dd HH:mm:ss.000") & "' >= Col2))")*-1/100`;
 /**
  * Returns the formatted rows for Export
  * @param row
